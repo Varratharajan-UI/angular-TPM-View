fd451e389ef8361de480be062bea3c21784ec458
Varratharajan-UI <114236118+Varratharajan-UI@users.noreply.github.com>
2022-09-25 00:42:06 +0530

    9 files modified

--- src/app/tpm-view-page/tpm-candidate-offer-extended/tpm-candidate-offer-extended.component.ts
+++ src/app/tpm-view-page/tpm-candidate-offer-extended/tpm-candidate-offer-extended.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tpm-candidate-offer-extended.component.scss']
 })
 export class TpmCandidateOfferExtendedComponent implements OnInit {
-
+  public joiningStatusOption = false;
+  public bvStatusOption = false;
+  public mainStatusOption = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  openOption(option) {
+    if(option == 'joiningbonus') {
+      this.joiningStatusOption = !this.joiningStatusOption;
+    } else if(option == 'backgroundcheck') {
+      this.bvStatusOption = !this.bvStatusOption;
+    } else if(option == 'offeroption') {
+      this.mainStatusOption = !this.mainStatusOption;
+    }
+
   }
 
 }
--- src/app/tpm-view-page/tpm-candidate-prospects/tpm-candidate-prospects.component.ts
+++ src/app/tpm-view-page/tpm-candidate-prospects/tpm-candidate-prospects.component.ts
@@ -1,19 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-tpm-candidate-prospects',
   templateUrl: './tpm-candidate-prospects.component.html',
-  styleUrls: ['./tpm-candidate-prospects.component.scss']
+  styleUrls: ['./tpm-candidate-prospects.component.scss'],
 })
 export class TpmCandidateProspectsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray("", event.previousIndex, event.currentIndex);
-  }
-
+  ngOnInit() {}
 }
