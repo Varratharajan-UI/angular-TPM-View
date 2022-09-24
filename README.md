60f478c78f18ae1f5884d1c708456fe16a8b06d6
Varratharajan-UI <114236118+Varratharajan-UI@users.noreply.github.com>
2022-09-24 12:41:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-scss-starter-ex23yh
+
+[Edit on StackBlitz ⚡️](https://jake.stackblitz.com/edit/angular-scss-starter-ex23yh)
---
+++ src/app/tpm-view-page/tpm-candidate-offer-extended/tpm-candidate-offer-extended.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tpm-candidate-offer-extended',
+  templateUrl: './tpm-candidate-offer-extended.component.html',
+  styleUrls: ['./tpm-candidate-offer-extended.component.scss']
+})
+export class TpmCandidateOfferExtendedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
